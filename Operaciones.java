362a4c002a34f6f6400711622a835e42f4212af5
Jose Pablo Ponce <46695351+JosePabloPonce@users.noreply.github.com>
2020-05-19 23:46:55 -0600

    Add files via upload

---
+++ Operaciones.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Operaciones {
+
+
+	ArrayList<String> Dicvalores = new ArrayList<String>();
+	
+	
+	
+
+
+	//Funcion para leer archivo de texto y pasarlo a array
+	 public ArrayList<String> leerarchivoalista(String nombredearchivodetexto) throws IOException 
+	    {
+	        FileReader leer = new FileReader(nombredearchivodetexto);         
+	        BufferedReader bufferedReader = new BufferedReader(leer);
+	        String leerlinea = null;
+	        
+	        while ((leerlinea = bufferedReader.readLine()) != null) {
+	        	
+	        	String[]ciudad;
+	        	
+
+	        
+	        
+	        
+	        }	         
+	        bufferedReader.close();
+	        return Dicvalores;
+	    }
+	 
+	 
+
+
+
+}
